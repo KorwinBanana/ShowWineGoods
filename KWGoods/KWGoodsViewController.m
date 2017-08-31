1c7f3169de7699c005851c07177e080140cde411
korwin <523404234@qq.com>
2017-08-31 13:23:23 +0800

    ShowWineGoods1.0.1

--- KWGoods/KWGoodsViewController.m
+++ KWGoods/KWGoodsViewController.m
@@ -19,7 +19,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.tableView = [[UITableView alloc] initWithFrame:self.tableView.frame style:UITableViewStyleGrouped];
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStyleGrouped];
+    self.tableView.dataSource = self;
+    //tableView高度
+    self.tableView.rowHeight = 88;
+    
+    //每一组的头部尾部高度
+    self.tableView.sectionHeaderHeight = 88;
+    self.tableView.sectionFooterHeight = 88;
+    
+    //整张表的头部尾部View
+//    self.tableView.tableHeaderView = [[UISwitch alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
+//    self.tableView.tableFooterView =;
+    
+    //分割线颜色
+//    self.tableView.separatorColor = [UIColor clearColor];
+    
+    //分割线样式
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
 }
 
 - (NSArray *) goodGroup
@@ -55,7 +72,25 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil];
+    //设置右边显示的样式
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    //设置右边显示的控件
+    cell.accessoryView = [[UISwitch alloc] init];
+    //设置选中样式
+    cell.selectionStyle = UITableViewCellSelectionStyleBlue;
+    //设置cell的背景颜色
+//    cell.backgroundColor = [UIColor redColor];
+    
+    //设置cell的背景View
+//    UIView *bg = [[UIView alloc]init];
+//    bg.backgroundColor = [UIColor blueColor];
+//    cell.backgroundView = bg;
+    
+    //设置cell的选中背景View
+//    UIView *bg = [[UIView alloc]init];
+//    bg.backgroundColor = [UIColor blueColor];
+//    cell.selectedBackgroundView = bg;
+
     
     KWGoodsGroup *goodsGroup = self.goodGroup[indexPath.row];
     
@@ -64,6 +99,16 @@
     cell.detailTextLabel.text = [NSString stringWithFormat:@"%@$",goodsGroup.money];
     
     return cell;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return @"头部";
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
+{
+    return @"尾部";
 }
 
 @end
